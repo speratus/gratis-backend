5aeafb56ad95493c80f05eac3255c5487ac2eeb3
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-06 17:37:34 -0500

    initial commit of rails and some models

---
+++ app/models/shoutout.rb
@@ -0,0 +1,4 @@
+class Shoutout < ApplicationRecord
+  belongs_to :post
+  belongs_to :recipient, class_name: 'User'
+end
---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    has_one_attached :avatar, :background
+end
