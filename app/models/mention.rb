7dfb8edd545b372a1e913bf8c6c99e670713f43a
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-06 17:59:14 -0500

    adds basic friendship and comment

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,4 +1,4 @@
 class Mention < ApplicationRecord
-  belongs_to :post
+  belongs_to :shoutout
   belongs_to :recipient, class_name: 'User'
 end
--- app/models/shoutout.rb
+++ app/models/shoutout.rb
@@ -1,3 +1,4 @@
 class Shoutout < ApplicationRecord
   belongs_to :creator, class_name: 'User'
+  has_many :mentions
 end
