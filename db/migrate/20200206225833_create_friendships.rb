26099dfea82c092a4fdb275398a334be4d936ca5
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-07 14:33:26 -0500

    removes unneccessary puts

--- db/migrate/20200206225833_create_friendships.rb
+++ db/migrate/20200206225833_create_friendships.rb
@@ -1,7 +1,6 @@
 class CreateFriendships < ActiveRecord::Migration[6.0]
   def change
     create_table :friendships do |t|
-      puts t.class
       t.belongs_to :follower, null: false, foreign_key: true, foreign_key: {to_table: :users}
       t.belongs_to :followee, null: false, foreign_key: true, foreign_key: {to_table: :users}
       t.string :status
