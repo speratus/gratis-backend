6065e4696ffded32e3a72b92f8b959f44947a348
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-08 15:26:58 -0500

    adds shoutouts controller and serializer

--- app/controllers/shoutouts_controller.rb
+++ app/controllers/shoutouts_controller.rb
@@ -1,16 +1,45 @@
 class ShoutoutsController < ApplicationController
+  before_action :authenticated?
+  before_action :get_authorized_user, except: [:index, :create]
+
+
   def index
   end
 
   def show
+    render json: @shoutout
   end
 
   def update
+    if @shoutout.update(shoutout_params)
+      render json @shoutout
+    else
+      render json: {message: 'Invalid shoutout parameters'}, status: 400
+    end
   end
 
   def create
+    shoutout = check_authorization(Shoutout.new(shoutout_params), current_user)
+
+    if shoutout.save
+      render json: shoutout
+    else
+      render json: {message: 'there was an error in creating the shoutout.'}, status: 400
+    end
   end
 
   def delete
+    @shoutout.destroy
+  end
+
+  private
+
+  def shoutout_params
+    params.require(:shoutout).permit(:content, :visibility, :mentions: [:user_id])
+  end
+
+  def get_authorized_shoutout
+    shoutout = Shoutout.find_by(id: params[:id])
+    @shoutout = check_authorization(shoutout, current_user)
   end
 end
--- app/models/shoutout.rb
+++ app/models/shoutout.rb
@@ -6,4 +6,18 @@ class Shoutout < ApplicationRecord
   has_many :shoutout_likes, dependent: :destroy
 
   has_many :comments, dependent: :destroy
+
+  accepts_nested_attributes_for :mentions
+
+  check_perm 'shoutouts#update', 'shoutouts#delete' do |shoutout, user|
+    shoutout.user == user
+  end
+
+  check_perm 'shoutouts#create' do |shoutout, user|
+    !user.nil?
+  end
+
+  check_perm 'shoutouts#show' do |shoutout, user|
+    shoutout.visibility == 'public' || shoutout.user.friends.include?(user)
+  end
 end
